910a04dacd256acba5a1df0be387cce476310519
Stevenson Nelson <sonson521@gmail.com>
2019-06-14 00:17:51 -0400

    turned the object into a string.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,46 @@
-// Global vars 
-var wins = 0; 
-var loses = 0;
-var randomNum = Math.floor(Math.random()*40) + 1;
-var crystalScore = 0;
+$(document).ready(function() {
+  // Global vars 
+var wins ; 
+var loses ;
+var randomNum = Math.floor(Math.random()*120) + 19;
+var randomCrystalNum = Math.floor(Math.random()*12) + 1;
+var crystalScore ;
 
-console.log(randomNum);
+// random num and start of the game 
+$('#generatedNum').html(randomNum);
+
+// random crystal value 
+
+$('.btn-img').on('click', function() {
+   parseInt( $('#totalScore').html(randomCrystalNum).val() );
+   
+  
+});
+
+
+// $('button#redCrystal').on('click', function() {
+//   $('#totalScore').html(randomCrystalNum);
+// });
+// $('button#blueCrystal').on('click', function() {
+//   $('#totalScore').html(randomCrystalNum);
+// });
+// $('button#purpleCrystal').on('click', function() {
+//   $('#totalScore').html(randomCrystalNum);
+// });
+// $('button#greenCrystal').on('click', function() {
+//   $('#totalScore').html(randomCrystalNum);
+// });
+
+
+// add the values to all buttons 
+
+
+
+
+
+
+})
+
+
+
+
